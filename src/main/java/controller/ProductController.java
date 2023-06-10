094af2abe0c8d9dbe7a029b8868653866164cb02
Keshav Kumar <keshav.k987@gmail.com>
2023-06-10 19:51:35 +0530

    first commit

---
+++ src/main/java/controller/ProductController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Product;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    
+    @GetMapping
+    public List<Product> allProducts(){
+        List<Product> products =  new ArrayList<>();
+        products.add(new Product(1, "Sports Shirt") );
+        return products;
+    }
+
+}
